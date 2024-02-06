d0b51a62a5c99966864aacd8446a9b03e42788d6
Aleksey Verin <verevaa@gmail.com>
2024-02-06 16:20:29 +0300

    fix: next config

--- next.config.mjs
+++ next.config.mjs
@@ -14,6 +14,12 @@ const nextConfig = {
         port: '1337',
         pathname: '/**',
       },
+      {
+        protocol: 'http',
+        hostname: '127.0.0.1',
+        port: '1337',
+        pathname: '/**',
+      },
     ],
   },
 }
